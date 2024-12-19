28888e98b4250f51c14f6a497f6232c7a3dc8457
Mani-420 <tahirabdulrehman46@gmail.com>
2024-12-19 22:57:14 +0500

    Module 1

---
+++ Modules/hello.js
@@ -0,0 +1,7 @@
+// const math = require("./math");
+const {addFn, subFn} = require("./math");
+
+// console.log(`Math value is: ${math(2, 4)}`);
+// console.log(math);
+console.log(`Addition is: ${addFn(2, 4)}`);
+console.log(`Subtraction is: ${subFn(2, 4)}`);
