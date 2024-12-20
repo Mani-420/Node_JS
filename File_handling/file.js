c6ebf6c2934250e7cf2b8dddde8725a5dee309fe
Mani-420 <tahirabdulrehman46@gmail.com>
2024-12-20 16:18:58 +0500

    Architecture of Node-JS

--- File_handling/file.js
+++ File_handling/file.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 
-
 // Synchronous writing
 fs.writeFileSync("./File_handling/test.txt", "Hello! This is a testing file.");
 
@@ -22,7 +21,6 @@ const result2 = fs.readFile("./File_handling/test2.txt", "utf-8", (err, result2)
 
 // Synchronous Appending
 const result3 = fs.appendFileSync("./File_handling/test.txt", "\n=> New Data added.");
-
 
 // Asynchronous Appending
 const result4 = fs.appendFile("./File_handling/test2.txt", "\nNew Data added.", (err) => {});
