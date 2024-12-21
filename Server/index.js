df89488ed46542889960f90f77dff1e28de138ef
Mani-420 <tahirabdulrehman46@gmail.com>
2024-12-21 16:54:21 +0500

    Main Server file

--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,19 @@
 const http = require("http");
 const fs = require('fs');
+const url = require("url");
 
 const myServer = http.createServer((req, res)=>{
+    if (req.url === "/favicon.ico") return res.end();
     const log = `${Date.now()}: ${req.url} New Req Received.\n`;
+    const myUrl = url.parse(req.url, true)
     fs.appendFile("log.txt", log, (err, data) => {
-        switch(req.url){
+        switch(myUrl.pathname){
             case '/':
                 res.end("HomePage");
                 break;
             case '/about':
-                res.end("I am Abdul Rehman Tahir.");
+                const username = myUrl.query.myname;
+                res.end(`Hi! ${username}`);
                 break;
             default:
                 res.end("404 Not Found");
